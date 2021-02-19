a304ccad9c5fced5820f1e06aa0f50ebc0875842
IsaiasGameDev <54710832+Isaiasgamedev@users.noreply.github.com>
2021-02-19 18:11:29 -0300

    Return

---
+++ Assets/Scripts/Level/ReturnPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnPosition : MonoBehaviour
+{
+
+	public Transform ReturnPositionNow;
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other)
+		{
+			if (other.GetComponent<Player>())
+			{
+				var x = other.GetComponent<Player>();
+				if (x)
+				{
+					x.gameObject.transform.position = ReturnPositionNow.position;
+				}
+			}
+		}
+	}
+	
+}
